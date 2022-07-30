1c7a218dfde6c16af931500e49e152d90116bbb3
Cheah Eng Teong <ceteongvanness@hotmail.com>
2022-07-30 22:11:29 +0800

    Update local-config.js

--- dist/local-config.js
+++ dist/local-config.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.publicUser = void 0;
 exports.publicUser = {
-    userName: 'zhoufeng@qq.fengyun.org',
-    userPassword: '075391@Cet'
+    userName: '<your email>',
+    userPassword: '<your password>'
 };
