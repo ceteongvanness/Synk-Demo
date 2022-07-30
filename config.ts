b0ca426baa1d401e78ab4d6b35b1c266e18ad5cf
cheahengteong <69237824+cheahengteong@users.noreply.github.com>
2022-07-30 19:02:28 +0800

    upload source code

---
+++ config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  publicUrl: 'https://app.scout.asia',
+  //publicUrl: 'https://beta.scout.asia',
+  //publicUrl: 'https://test.scout.asia',
+  portalUrl: 'https://staging-portal.scout.asia',
+  //portalUrl: 'https://test-portal-b.scout.asia/',
+  connectsUrl: 'https://connects.scout.asia',
+};
---
+++ local-config.ts
@@ -0,0 +1,10 @@
+<<<<<<< Updated upstream
+export const publicUser = {
+    userName: 'zhoufeng@qq.fengyun.org',
+    userPassword: '075391@Cet'
+=======
+export const publicUser = { 
+    "userName": "v-seaxiao@handshakes.com.sg",
+    "userPassword": "Taylor%1989@#&5768"
+>>>>>>> Stashed changes
+}
