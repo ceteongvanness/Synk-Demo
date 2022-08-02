2008b3e2099aa47202d9c23d12921c01a897ab51
cheahengteong <69237824+cheahengteong@users.noreply.github.com>
2022-08-02 11:06:59 +0800

    Update local-config.ts

--- local-config.ts
+++ local-config.ts
@@ -1,10 +1,7 @@
-<<<<<<< Updated upstream
 export const publicUser = {
     userName: 'zhoufeng@qq.fengyun.org',
     userPassword: '075391@Cet'
-=======
 export const publicUser = { 
     "userName": "v-seaxiao@handshakes.com.sg",
     "userPassword": "Taylor%1989@#&5768"
->>>>>>> Stashed changes
 }
